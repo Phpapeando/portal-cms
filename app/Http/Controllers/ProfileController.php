404364956d6eded863c893e92bcaa3078d776234
Fabio Ricart <fabio.ricart@hotmail.com>
2024-07-05 17:01:05 -0400

    crio controller de perfil e metodo index

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Profile;
+use Illuminate\Http\Request;
+
+class ProfileController extends Controller
+{
+    public function index()
+    {
+        $profile = Profile::paginate(15);
+
+        return view('profiles.index', compact('profile'));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -11,3 +12,5 @@ Route::post('/users', [UserController::class, 'store'])->name('users.store');
 Route::get('/users/{id}/edit', [UserController::class, 'edit'])->name('users.edit');
 Route::put('/users/{id}', [UserController::class, 'update'])->name('users.update');
 Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('users.destroy');
+
+Route::get('/profiles', [ProfileController::class, 'index'])->name('profiles.index');
